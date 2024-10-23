459653376f7ae77962fc2b89a1d5f8b2332f27fb
juliaooliveira <148073511+juliaooliveira@users.noreply.github.com>
2024-10-23 10:23:30 -0300

    Update informacoesGlobais.js

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,6 +3,10 @@ const url = "https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 async function visualizarInformacoesGlobais() {
   const res = await fetch(url)
   const dados = await res.json()
+  const pessosasConectdas = (dados.total_pessoas_conectadas /le9)
+  const pessoasNoMundo = (dados.total_pessoas_mundo /le9)
+  const horas = parseInt(dados.tempo_medio)
+  const minutos = Math.round((dados.tempo_medio - horas
   console.log(dados);
 }
 
