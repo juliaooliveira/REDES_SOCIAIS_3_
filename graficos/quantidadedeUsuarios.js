2a6fedf32087a3a2a216ca8c9be955dbb55ad729
juliaooliveira <148073511+juliaooliveira@users.noreply.github.com>
2024-11-14 08:42:15 -0300

    Update quantidadedeUsuarios.js

--- graficos/quantidadedeUsuarios.js
+++ graficos/quantidadedeUsuarios.js
@@ -1,1 +1,22 @@
+async function quantidadeUsuariosPorRede() {
+  const url =  'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+  const res = await fetch(url)
+  const dados = await res.json()
+  const nomeDasRedes = Object.key(dados)
+  const quantidadeDeUsaurios = Object.values(dados)
 
+  const data = [
+    {
+      x: nomeDasRedes,
+      y: quantidadeDeUsuarios,
+      type: 'bar'
+    }
+  ]
+
+  const grafico = document.createElement('div')
+  grafico.className = 'grafico'
+  document.getElementByld('graficos-container').appendChild(grafico)
+  Plotly.newPlot(grafico, data)
+}
+
+quantidadeUsuariosPoronde()
